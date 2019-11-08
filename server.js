f9fe9df3b8d2fdef8dc22056b8b9d05c9a30a1b3
Natalia Titova <bnmlucky@gmail.com>
2019-11-08 16:33:01 -0500

    correcting starting page for heroku

--- server.js
+++ server.js
@@ -23,6 +23,9 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use("/todos", todosController);
+app.get("/", (req, res) => {
+    res.redirect("/todos")
+})
 
 
 // Listener
